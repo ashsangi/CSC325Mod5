349e8b1773ab332eedc0d9447109783fe0b34dae
sanga31 <sanga31@farmingdale.edu>
2024-06-26 14:52:05 -0400

    Complete

--- src/main/java/com/example/csc325mod5/HelloApplication.java
+++ src/main/java/com/example/csc325mod5/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1125, 679);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("FSC CSC_325 Full Stack Project");
         stage.setScene(scene);
         stage.show();
     }
